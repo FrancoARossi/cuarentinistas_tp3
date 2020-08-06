c9b718d03d5e9654f5c78b78a47f7de635c06596
FrancoARossi <rossifrancoa@gmail.com>
2020-08-06 19:51:55 -0300

    Conectando llamadas REST de la app con las de nuestro servicio

--- app/src/main/java/com/example/cuarentinistas_tp3/MainActivity.java
+++ app/src/main/java/com/example/cuarentinistas_tp3/MainActivity.java
@@ -8,17 +8,18 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
-    // Ejemplo de nueva actividad
-    protected void showLayoutTest(View view) {
-        Intent intent = new Intent(this, LayoutTest.class);
-        startActivity(intent);
-    }
+    // Usar la IPv4 correspondiente a la PC que se encuentra ejecutando el servidor REST.
+    String localServerAdress = "ACA-VA-LA-IPv4:8080";
 
-    public void restTest(View view) {
-        new MiAsyncTask().execute();
-    }
+    // Ejemplo de nueva actividad
+//    protected void showLayoutTest(View view) {
+//        Intent intent = new Intent(this, LayoutTest.class);
+//        startActivity(intent);
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,20 +27,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void getClient1(View view) {
+        new restCall().execute("GET", "http://"+localServerAdress+"/cuarentinistas/rest/clientes/1");
+    }
 
+    public void restTest(View view) {
+        new restCall().execute("GET", "https://jsonplaceholder.typicode.com/todos/1");
+    }
 
-    private class MiAsyncTask extends AsyncTask<String, String, String> {
+    private class restCall extends AsyncTask<String, String, String> {
         @Override
-        protected String doInBackground(String... strings) {
-            return RESTService.makeGetRequest(
-                    "https://reqres.in/api/users/2");
+        protected String doInBackground(String... args) {
+            String callType = args[0];
+            String url = args[1];
+
+            switch (callType) {
+                case "GET":
+                    return RESTService.makeGetRequest(url);
+                case "POST":
+                    return RESTService.callREST(url, "POST", null);
+            }
+            return null;
         }
 
         @Override
         protected void onPostExecute(String result) {
-            Toast notificacion = Toast.makeText(
+            Toast notification = Toast.makeText(
                     getApplicationContext(), result, Toast.LENGTH_LONG);
-            notificacion.show();
+            notification.show();
         }
     }
 }
