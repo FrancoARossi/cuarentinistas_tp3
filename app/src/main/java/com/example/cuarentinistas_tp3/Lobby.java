6e6a0019e057933bd1056f34c7f57c0b0cb18f67
FrancoARossi <rossifrancoa@gmail.com>
2020-08-19 18:00:26 -0300

    Creando tabla dinamica de UltimosMovimientos

--- app/src/main/java/com/example/cuarentinistas_tp3/Lobby.java
+++ app/src/main/java/com/example/cuarentinistas_tp3/Lobby.java
@@ -1,12 +1,16 @@
 package com.example.cuarentinistas_tp3;
 
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+class ServerAddress {
+    public static String value() {
+        return "192.168.0.217:8080/cuarentinistas";
+    }
+}
 
 public class Lobby extends AppCompatActivity {
 
--- app/src/main/java/com/example/cuarentinistas_tp3/UltimosMovimientos.java
+++ app/src/main/java/com/example/cuarentinistas_tp3/UltimosMovimientos.java
@@ -1,8 +1,22 @@
 package com.example.cuarentinistas_tp3;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Iterator;
 
 public class UltimosMovimientos extends AppCompatActivity {
 
@@ -10,5 +24,35 @@ public class UltimosMovimientos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ultimos_movimientos);
+        asyncCall getUltimosMovimientos = new asyncCall();
+        getUltimosMovimientos.execute();
+    }
+
+
+    private class asyncCall extends AsyncTask<String, String, String> {
+        @Override
+        protected String doInBackground(String... args) {
+            //return RESTService.makeGetRequest(ServerAddress.value()+"/rest/movimientos");
+            return RESTService.makeGetRequest("https://jsonplaceholder.typicode.com/todos/1");
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            try {
+                JSONObject data = new JSONObject(result);
+                TableLayout table = (TableLayout) findViewById(R.id.ult_movs_table);
+                TableRow row = new TableRow(getApplicationContext());
+                for (Iterator key = data.keys(); key.hasNext();) {
+                    TextView text1 = new TextView(getApplicationContext());
+                    text1.setText(data.get((String) key.next()).toString());
+                    text1.setTextColor(Color.BLACK);
+                    text1.setTextSize(14);
+                    row.addView(text1);
+                }
+                table.addView(row);
+            } catch (JSONException e) {
+                //some exception handler code.
+            }
+        }
     }
 }
